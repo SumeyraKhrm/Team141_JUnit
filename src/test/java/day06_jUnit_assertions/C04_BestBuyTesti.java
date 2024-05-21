c5503fd7851fb29773927d390b4f530d01540a55
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-05-21 19:51:48 +0200

    version

--- src/test/java/day06_jUnit_assertions/C04_BestBuyTesti.java
+++ src/test/java/day06_jUnit_assertions/C04_BestBuyTesti.java
@@ -1,8 +1,9 @@
 package day06_jUnit_assertions;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -11,10 +12,10 @@ public class C04_BestBuyTesti {
 
      // https://www.bestbuy.com/ Adresine gidin farkli test method’lari olusturarak
      //asagidaki testleri yapin
-     //○ Sayfa URL’inin https://www.bestbuy.com/ ‘a esit oldugunu test edin// titleTest
-     //=> Sayfa başlığının “Rest” içermediğini(contains) test edin
-     // logoTest => BestBuy logosunun görüntülendigini test edin
-     // FrancaisLinkTest => Fransizca Linkin görüntülendiğini test edin
+     //Sayfa URL’inin https://www.bestbuy.com/ ‘a esit oldugunu test edin
+     //titleTest=> Sayfa başlığının “Rest” içermediğini(contains) test edin
+     //logoTest => BestBuy logosunun görüntülendigini test edin
+     //FrancaisLinkTest => Fransizca Linkin görüntülendiğini test edin
 
     static WebDriver driver ;
 
@@ -32,10 +33,50 @@ public class C04_BestBuyTesti {
     }
 
 
+    @Test @Order(1)
+    // https://www.bestbuy.com/ Adresine gidin ve sayfanın bu URL'e esit oldugunu test edin
+    public void anasayfaTesti() throws InterruptedException {
+        driver.get(" https://www.bestbuy.com/");
+
+        String expectedUrl ="https://www.bestbuy.com/";
+        String actualUrl = driver.getCurrentUrl();
+
+        Assertions.assertEquals(expectedUrl,actualUrl);
+    }
+
+    //titleTest => Sayfa başlığının “Rest” içermediğini(contains) test edin
+
+    @Test @Order(2)
+    public void titleTest(){
+
+        String expectedTitleIcerik = "Rest";
+        String actualTitle = driver.getTitle();
+
+        Assertions.assertFalse(actualTitle.contains(expectedTitleIcerik));
+    }
 
 
 
+    //	○ logoTest => BestBuy logosunun görüntülendigini test edin
+    @Test @Order(3)
+    public void logoTest(){
+
+        WebElement logoElementi = driver.findElement(By.xpath("(//img[@alt='Best Buy Logo'])[1]"));
+        Assertions.assertTrue(logoElementi.isDisplayed());
+    }
 
 
+    @Test @Order(4)
+    //	○ FrancaisLinkTest => Fransizca Linkin görüntülendiğini test edin
 
-}
+    public void FrancaisLinkTest(){
+
+        WebElement francaisLinkElementi = driver.findElement(By.xpath("//*[text()='Français']"));
+
+        Assertions.assertTrue(francaisLinkElementi.isDisplayed());
+
+
+         }
+
+
+    }
