22274566eded6a21baebf7e311a6475823b242d1
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-05-23 10:25:33 +0200

    version

---
+++ src/test/java/day08_jsAlerts_iFrame/C02_BasicAuthentication.java
@@ -0,0 +1,26 @@
+package day08_jsAlerts_iFrame;
+
+import org.junit.jupiter.api.Test;
+
+public class C02_BasicAuthentication {
+
+    @Test
+    public void test01(){
+
+        //1- Bir class olusturun : BasicAuthentication
+        //2- https://testotomasyonu.com/basicauth sayfasina gidin
+
+        // driver.get("https://testotomasyonu.com/basicauth");
+        // URL'e normal bir sekilde gitmek istersek
+        // sonrasinda kullanici adi ve sifreyi otomasyonla yazmamiz  MUMKUN DEGILDIR
+
+
+        // Otomasyonla giris yapabilmek icin
+        // giris yapmak istedigimiz firmadan
+        // kullanici adi ve sifre ile birlikte
+        // bu degerleri nasil yollayacagimiz BILGISINI de almamiz
+        // ve bize soylenen yontemle giris yapmamiz ZORUNLUDUR
+
+
+    }
+ }
