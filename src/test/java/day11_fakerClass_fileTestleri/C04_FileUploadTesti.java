917e39dd2c5002651c86888c3126fa8faa6f6c94
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-05-27 18:24:27 +0200

    version

--- src/test/java/day11_fakerClass_fileTestleri/C04_FileUploadTesti.java
+++ src/test/java/day11_fakerClass_fileTestleri/C04_FileUploadTesti.java
@@ -1,4 +1,56 @@
 package day11_fakerClass_fileTestleri;
 
-public class C04_FileUploadTesti {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+public class C04_FileUploadTesti extends TestBase {
+
+
+
+    @Test
+    public void test(){
+
+        //https://the-internet.herokuapp.com/upload adresine gidelim
+        driver.get("https://the-internet.herokuapp.com/upload");
+        ReusableMethods.bekle(1);
+
+        //chooseFile butonuna basalim
+        //upload.png dosyasini secelim.
+
+        /*
+            chooseFile butonuna bastigimizda,
+            bilgisayarimizdaki dosyalar acilir
+            driver'in bilgisayarimizdaki dosyalara erisimi YOKTUR
+
+            bunun yerine
+            selenium kullanarak dosya secimi yapmak istedigimizde
+            chooseFile butonunu locate edip
+            yukleyecegimiz dosyanin dosyaYolunu sendKeys() ile yollariz
+         */
+
+        String yuklenecekDosyaYolu = "/Users/erolkahraman/IdeaProjects/Team141_JUnit/src/test/java/day11_fakerClass_fileTestleri/selenium-snapshot.png";
+
+        WebElement chooseFileButtonu = driver.findElement(By.id("file-upload"));
+
+        chooseFileButtonu.sendKeys(yuklenecekDosyaYolu);
+        ReusableMethods.bekle(5);
+
+        //Upload butonuna basalim.
+        driver.findElement(By.id("file-submit"))
+                .click();
+
+        //“File Uploaded!” textinin goruntulendigini test edelim.
+        String expectedYazi = "File Uploaded!";
+        String actualYazi = driver.findElement(By.tagName("h3")).getText();
+
+        Assertions.assertEquals(expectedYazi,actualYazi);
+
+        ReusableMethods.bekle(2);
+
+
+    }
 }
--- src/test/java/day11_fakerClass_fileTestleri/C05_DinamikFileExistTesti.java
+++ src/test/java/day11_fakerClass_fileTestleri/C05_DinamikFileExistTesti.java
@@ -1,4 +1,38 @@
 package day11_fakerClass_fileTestleri;
 
-public class C05_DinamikFileExistTesti {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import utilities.TestBase;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class C05_DinamikFileExistTesti extends TestBase {
+
+    @Test
+    public void test01(){
+
+        // masaustumuzde selenium-snapshot.png isimli bir dosya oldugunu test edin
+
+
+       String dosyaYolu = "/Users/erolkahraman/Downloads/selenium-snapshot.png";
+
+        Assertions.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+
+
+        //       /Users/ahmetbulutluoz                       /Desktop/upload.png
+        //      /Users/burhanakyuz                          /Desktop/upload.png
+
+        // Tum bilgisayarlarda tanimli bir anaDosyaYolu vardir
+        // Java bu temel dosya yolunu bize veren bir koda sahiptir
+
+        System.out.println(System.getProperty("user.home"));  // /Users/erolkahraman
+
+        dosyaYolu =System.getProperty("user.home") + "/Downloads/selenium-snapshot.png";
+
+        Assertions.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+
+
+    }
 }
