52da92365022df4a8ba8c74f00f57e31a694ec75
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-05-30 13:30:15 +0200

    version

---
+++ src/test/java/day15_getScreenshot/C03_GetScreenshotMethodKullanma.java
@@ -0,0 +1,4 @@
+package day15_getScreenshot;
+
+public class C03_GetScreenshotMethodKullanma {
+}
--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,8 +1,13 @@
 package utilities;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -44,6 +49,7 @@ public class ReusableMethods {
                 break;
             }
         }
+
 
 
 
